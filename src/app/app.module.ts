4ff42c69968e7908faa506f501dab2526bf4f26e
unknown <victor.ibarra.olivares@gmail.com>
2022-06-29 11:03:31 -0400

    Consumiendo Servicio (Poke API)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PokemonsComponent } from './components/pokemons/pokemons.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PokemonsComponent } from './components/pokemons/pokemons.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
